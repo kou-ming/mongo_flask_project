aef6ff17aca53d67d14c329127829928e6aa5fbf
kou-ming <t0937993806@gmail.com>
2024-11-01 01:44:25 +0800

    search_update

--- main_flask.py
+++ main_flask.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import url_for,redirect, render_template, request
+from flask import url_for,redirect, render_template, request, jsonify
 import pymongo
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
@@ -8,6 +8,30 @@ mydb = myclient["song"]
 
 
 app = Flask(__name__)
+
+def or_filter(or_filter_list):
+    print('or_filter')
+    song_list = []
+    mysong = mydb["song_info"]
+    for or_song_name in or_filter_list:
+        for song in mysong.find({"Tag": or_song_name}):
+            if song not in song_list:
+                # print(song)
+                song_list.append(song)
+    return song_list
+
+def and_filter(song_list, and_filter_list):
+    print('and_filter\n')
+    new_song_list = song_list
+    for and_song_tag in and_filter_list:
+        new_song_list = []
+        for song in song_list:
+            if and_song_tag in song['Tag']:
+                new_song_list.append(song)
+        song_list = new_song_list
+    print('and_filter_end')
+    return new_song_list
+
 
 @app.route('/insert', methods = ['GET', 'POST'])
 def insert():
@@ -30,6 +54,7 @@ def insert():
         AuthName = request.values['AuthName']
         LikeLevel = request.values['LikeLevel']
         Select_Tag = request.form.getlist('Tag')
+        Select_Tag.append(AuthName)
         if SongName == '' or AuthName == '' :
             info = '歌曲名或作者名不得為空'
         else:
@@ -64,6 +89,7 @@ def tag():
 
 @app.route('/songlist', methods = ['GET', 'POST'])
 def songlist():
+    info = '尚未選擇任何選項'
     mysong = mydb['song_info']
     song_list = []
     for song in mysong.find():
@@ -79,10 +105,58 @@ def songlist():
     for auth in myauth.find():
         auth_list.append(auth['AuthTag'])
 
-    # if request.method == 'POST':
-    #     TagName = request.values['TagName']
 
-    return render_template('songlist.html', songlist = song_list, song_num = len(song_list), taglist = tag_list, authlist = auth_list)
+    if request.method == 'POST':
+        info = ''
+        data = request.get_json()
+        button_values = data.get('buttons', [])
+        or_filter_list = []
+        and_filter_list = []
+        pre_filter = 0
+        input_num = 0;
+        # print("接收到的按鈕值:", button_values)
+        flag = True     #排序是否有錯
+        for button_name in button_values:
+            if button_name[0] != str(input_num):
+                info = '過濾器排序有問題'
+                flag = False
+                break
+            else:
+                info += button_name[2:] + ' '
+                print(button_name[2:])
+            input_num = (input_num+1)%2
+
+            if button_name[0] == '1':
+                if button_name[2:] == 'OR':
+                    pre_filter = 0
+                elif button_name[2:] == 'AND':
+                    pre_filter = 1
+            else:
+                if pre_filter == 0:
+                    or_filter_list.append(button_name[2:])
+                elif pre_filter == 1:
+                    and_filter_list.append(button_name[2:])
+        if len(button_values) != 0 and flag:
+            song_list = []
+            song_list = or_filter(or_filter_list)
+            song_list = and_filter(song_list, and_filter_list)
+        elif not flag:
+            song_list = []
+        else:
+            song_list = []
+            for song in mysong.find():
+                song_list.append(song)
+            info = '全歌單'
+        for song in song_list:
+            song['_id'] = str(song['_id'])
+        response_data = {
+            'songs': song_list,
+            'len': len(song_list),
+            'info': info
+        }
+        return jsonify(response_data)
+
+    return render_template('songlist.html', songlist = song_list, song_num = len(song_list), taglist = tag_list, authlist = auth_list, info=info)
 
 # @app.route('/')
 # def index():
@@ -105,6 +179,11 @@ def home():
 
 @app.route('/test', methods=['GET', 'POST'])
 def test():
+    if request.method == 'POST':
+        data = request.get_json()
+        button_values = data.get('buttons', [])
+        print("接收到的按鈕值:", button_values)
+
     return render_template('test.html')
 
 # @app.route('/home', methods=['GET', 'POST'])
